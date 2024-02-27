c57e3a58cffdcf4e4c621d4dba3c07bcd42d0057
Vũ Nguyễn <42742971+vunguyen22271@users.noreply.github.com>
2024-02-27 08:35:38 +0000

    feat: first init

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,26 @@
-# TODO: Designate a cloud provider, region, and credentials
 
+# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+    region = "us-east-1"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "udacity_t2" {
+    count         = 4
+    instance_type = "t2.micro"
+    ami           = "ami-0c7217cdde317cfec"
+    tags = {
+        Name = "Udacity T2"
+    }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "udacity_m4" {
+    count         = 2
+    instance_type = "m4.large"
+    ami           = "ami-0c7217cdde317cfec"
+    tags = {
+        Name = "Udacity M4"
+    }
+}
+# terraform destroy -target aws_instance.udacity_m4
